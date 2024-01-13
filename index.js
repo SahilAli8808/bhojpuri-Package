8036e7e23023384af27d8058ce7f9a888c327c59
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-01-14 00:35:23 +0530

    Refactor code: Add Joda and ultaKara functions, update package version

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const  kareReBabu=  function karerebabu() {
-    console.log('Kare Re Babu! Thik ba?')
-}
-const Joda = function(a, b) {
-    return `la jod dehni: ${a} + ${b} = ${a + b}`;
+const kareReBabu = function karerebabu() {
+    console.log('Kare Re Babu! Thik ba?');
+
+    const Joda = function(a, b) {
+        return `la jod dehni: ${a} + ${b} = ${a + b}`;
+    };
+
+    const ultaKara = function(a) {
+        return `la ulta kar Dehni: ${a} = ${a.split('').reverse().join('')}`;
+    };
+
+   return {
+    Joda: Joda,
+    ultaKara : ultaKara
+   }
 };
-console.log(Joda(2, 3));
-const ultaKara = function(a) {
-    return `la ulta kar Dehni: ${a} = ${a.split('').reverse().join('')}`;
-}
-console.log(ultaKara('Babu'));
-module.exports = kareReBabu
+
+// Export both kareReBabu function and Joda function
+module.exports = kareReBabu;
